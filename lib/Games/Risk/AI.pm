52d98d7b8726987021a5fbed69d6d806a80d358f
Jérôme Quelin <jquelin@gmail.com>
2008-08-30 11:01:07 +0200

    constructor written by hand to assign a random name to the ai

--- lib/Games/Risk/AI.pm
+++ lib/Games/Risk/AI.pm
@@ -13,8 +13,65 @@ use 5.010;
 use strict;
 use warnings;
 
-use base qw{ Class::Accessor::Fast };
+use Carp;
+use List::Util qw{ shuffle };
+use Readonly;
 
+use base qw{ Class::Accessor::Fast };
+__PACKAGE__->mk_accessors( qw{ player } );
+
+my @NAMES = shuffle (
+    'Napoleon',             # france,   1769  - 1821
+    'Staline',              # russia,   1878  - 1953
+    'Alexander the Great',  # greece,   356BC - 323BC
+    'Julius Caesar',        # rome,     100BC - 44BC
+    'Attila',               # hun,      406   - 453
+    'Genghis Kahn',         # mongolia, 1162  - 1227
+    'Charlemagne',          # france,   747   - 814
+    'Saladin',              # iraq,     1137  - 1193
+);
+my $Id_name = 0;
+
+
+#--
+# CLASS METHODS
+
+# -- constructor
+
+#
+# my $ai = Games::Risk::AI::$AItype->new( \%params );
+#
+# Note that you should not instantiate a Games::Risk::AI object directly:
+# instantiate an AI subclass.
+#
+# Create a new AI of type $AItype. All subclasses accept the following
+# parameters:
+#  - player: the Game::Risk::Player associated to the AI. (mandatory)
+#
+# Note that the AI will automatically get a name, and update the player object.
+#
+sub new {
+    my ($pkg, $args) = @_;
+
+    # assign a new color
+    my $nbnames = scalar(@NAMES);
+    croak "can't assign more than $nbnames names" if $Id_name >= $nbnames;
+    my $name = $NAMES[ $Id_name++ ];
+
+    # create the object
+    my $self = bless $args, $pkg;
+
+    # update other object attributes
+    $self->player->name( $name );
+
+    return $self;
+}
+
+
+#--
+# METHODS
+
+# -- public methods
 
 sub description {
     my ($self) = @_;
@@ -50,6 +107,32 @@ This module is the base class for all artificial intelligence.
 
 
 =head1 METHODS
+
+=head2 Constructor
+
+
+Note that you should not instantiate a C<Games::Risk::AI> object directly:
+instantiate an AI subclass.
+
+
+=over 4
+
+=item * my $ai = Games::Risk::AI::$AItype->new( \%params )
+
+Create a new AI of type C<$AItype>. All subclasses accept the following parameters:
+
+=over 4
+
+=item * player: the C<Game::Risk::Player> associated to the AI. (mandatory)
+
+=back
+
+
+Note that the AI will automatically get a name, and update the player object.
+
+
+=back
+
 
 =head2 Object methods
 
