88350918f6d0c07b52c6cf03cc3f1b2cda61ee72
Jérôme Quelin <jquelin@gmail.com>
2008-09-02 17:59:52 +0200

    renamed player-active into empty16.png

--- lib/Games/Risk/GUI/Board.pm
+++ lib/Games/Risk/GUI/Board.pm
@@ -345,7 +345,7 @@ sub _onpriv_start {
     # FIXME: this should be in a sub/method somewhere
     my $path = find_installed(__PACKAGE__);
     my (undef, $dirname, undef) = fileparse($path);
-    $h->{images}{inactive}  = $top->Photo(-file=>"$dirname/icons/player-inactive.png");
+    $h->{images}{inactive}  = $top->Photo(-file=>"$dirname/icons/empty16.png");
     $h->{images}{active}    = $top->Photo(-file=>"$dirname/icons/player-active.png");
     $h->{images}{"dice_$_"} = $top->Photo(-file=>"$dirname/icons/dice-$_.png") for 0..6;
 
