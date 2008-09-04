3ead194bf6095cfc8226ac888512271b8066bb26
Jérôme Quelin <jquelin@gmail.com>
2008-09-04 17:02:49 +0200

    v0.3.3
     - continent bonus armies
     - fix warnings when attacking with only one dice
     - fix bug due to space launching attacks while current attack not done
     - bindings enhancements in invasion window
     - game does not prompt anymore for army move if no choice in number of
       armies to move

--- lib/Games/Risk.pm
+++ lib/Games/Risk.pm
@@ -25,7 +25,7 @@ use aliased 'POE::Kernel' => 'K';
 
 
 # Public variables of the module.
-our $VERSION = '0.3.2';
+our $VERSION = '0.3.3';
 
 Readonly my $ATTACK_WAIT => 0.300; # FIXME: hardcoded
 Readonly my $TURN_WAIT => 0.300; # FIXME: hardcoded
