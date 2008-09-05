186e98923cd7554a7fd1fe15a77c131b331b5f46
Jérôme Quelin <jquelin@gmail.com>
2008-09-05 15:47:17 +0200

    fix crash when clicking on non-territory

--- lib/Games/Risk/GUI/Board.pm
+++ lib/Games/Risk/GUI/Board.pm
@@ -832,10 +832,10 @@ sub _ongui_canvas_move_armies_from {
 
     my $curplayer = $h->{curplayer};
     my $country   = $h->{country};
-    my $id        = $country->id;
 
     # checks...
     return unless defined $country;
+    my $id = $country->id;
     return if $country->owner->name ne $curplayer->name; # country owner
     return if $country->armies - ($h->{fake_armies_out}{$id}//0) == 1;
 
