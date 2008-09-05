52041b9b96950a92617674ce7760b2ee8f0385c9
Jérôme Quelin <jquelin@gmail.com>
2008-09-05 16:09:06 +0200

    v0.4.0
     - possibility to move armies at the end of turn

--- lib/Games/Risk.pm
+++ lib/Games/Risk.pm
@@ -25,7 +25,7 @@ use aliased 'POE::Kernel' => 'K';
 
 
 # Public variables of the module.
-our $VERSION = '0.3.3';
+our $VERSION = '0.4.0';
 
 Readonly my $ATTACK_WAIT => 0.300; # FIXME: hardcoded
 Readonly my $TURN_WAIT => 0.300; # FIXME: hardcoded
