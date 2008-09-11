61088ce40c1e6e875c6e4abc81ca004a6d3908a1
Jérôme Quelin <jquelin@gmail.com>
2008-09-11 10:42:51 +0200

    v0.5.3
     - adding missing prereq (cf 0.5.2)

--- lib/Games/Risk.pm
+++ lib/Games/Risk.pm
@@ -20,7 +20,7 @@ use POE;
 use aliased 'POE::Kernel' => 'K';
 
 # Public variables of the module.
-our $VERSION = '0.5.2';
+our $VERSION = '0.5.3';
 
 use base qw{ Class::Accessor::Fast };
 __PACKAGE__->mk_accessors( qw{
