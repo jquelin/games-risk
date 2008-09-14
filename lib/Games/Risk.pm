8910e0e99f4bfdfba0853f279c301ed60437196a
Jérôme Quelin <jquelin@gmail.com>
2008-09-14 12:36:37 +0200

    v0.6.1
     - stealing cards from player who just lost
     - small internals cleanup

--- lib/Games/Risk.pm
+++ lib/Games/Risk.pm
@@ -20,7 +20,7 @@ use POE;
 use aliased 'POE::Kernel' => 'K';
 
 # Public variables of the module.
-our $VERSION = '0.6.0';
+our $VERSION = '0.6.1';
 
 use base qw{ Class::Accessor::Fast };
 __PACKAGE__->mk_accessors( qw{
