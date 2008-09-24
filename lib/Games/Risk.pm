4ff833ea6d493526c08ab0b11e5cb1e1d0e6abda
Jérôme Quelin <jquelin@gmail.com>
2008-09-24 18:03:08 +0200

    v1.0.3
     - faster background rendering
     - bugfix: prevent user to reattack till he invaded the country

--- lib/Games/Risk.pm
+++ lib/Games/Risk.pm
@@ -20,7 +20,7 @@ use POE;
 use aliased 'POE::Kernel' => 'K';
 
 # Public variables of the module.
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.3';
 
 use base qw{ Class::Accessor::Fast };
 __PACKAGE__->mk_accessors( qw{
