48dcdf2138bdb91e5add06d47c62494cc2578dc0
Jérôme Quelin <jquelin@gmail.com>
2008-10-21 20:07:22 +0200

    v2.0.2
    - standard mechanism to load bundled resources: GR::Resources

--- lib/Games/Risk.pm
+++ lib/Games/Risk.pm
@@ -20,7 +20,7 @@ use POE;
 use aliased 'POE::Kernel' => 'K';
 
 # Public variables of the module.
-our $VERSION = '2.0.1';
+our $VERSION = '2.0.2';
 
 use base qw{ Class::Accessor::Fast };
 __PACKAGE__->mk_accessors( qw{
