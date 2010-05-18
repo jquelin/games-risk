8bc335df2cabf00a295cc2e8784cfd8327f4f111
Gabor Szabo <szabgab@gmail.com>
2010-05-18 15:36:08 +0300

    add Help/About

--- lib/Games/Risk/GUI/Board.pm
+++ lib/Games/Risk/GUI/Board.pm
@@ -17,6 +17,7 @@ use Tk::JPEG;
 use Tk::PNG;
 use Tk::Sugar;
 
+use Games::Risk::GUI::About;
 use Games::Risk::GUI::Cards;
 use Games::Risk::GUI::Continents;
 use Games::Risk::GUI::GameOver;
@@ -68,6 +69,7 @@ sub spawn {
             _canvas_place_armies_initial   => \&_ongui_canvas_place_armies_initial,
             _canvas_motion                 => \&_ongui_canvas_motion,
             _quit                          => \&_quit,
+            _show_about                    => \&_show_about,
             _show_cards                    => \&_show_cards,
             _show_continents               => \&_show_continents,
             _window_close                  => \&_ongui_window_close,
@@ -588,6 +590,13 @@ sub _onpriv_start {
     );
     $top->bind('<F6>', $s->postback('_show_continents'));
 
+    my $help = $menubar->cascade(-label => T('~Help'));
+    $help->command(
+        -label       => T('~About'),
+        -command     => $s->postback('_show_about'),
+#        -image       => image('icon-cards'),
+        -compound    => 'right',
+    );
 
 
     #$h->{menu}{view} = $menubar->entrycget(1, '-menu');
@@ -727,6 +736,7 @@ sub _onpriv_start {
     Games::Risk::GUI::Cards->spawn({parent=>$top});
     Games::Risk::GUI::Continents->spawn({parent=>$top});
     Games::Risk::GUI::MoveArmies->spawn({parent=>$top});
+    Games::Risk::GUI::About->spawn({parent=>$top});
 
     #-- say that we're done
     K->yield('load_map', $args->{map});
@@ -1232,6 +1242,15 @@ sub _show_continents {
     K->post('continents', 'visibility_toggle');
 }
 
+#
+# _show_about()
+#
+# request Help/About window to be shown/hidden.
+#
+sub _show_about {
+    warn "_show_about\n";
+    K->post('about', 'visibility_toggle');
+}
 
 #
 # event: _window_close()
