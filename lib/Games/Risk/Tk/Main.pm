0a96e3c48615c29a8621dc69a28c58a2cabad9cf
Jerome Quelin <jquelin@gmail.com>
2011-07-25 14:43:08 +0200

    creating menu and toolbar

--- lib/Games/Risk/Tk/Main.pm
+++ lib/Games/Risk/Tk/Main.pm
@@ -13,6 +13,7 @@ use Readonly;
 use Tk;
 use Tk::Action;
 use Tk::Balloon;
+use Tk::ToolBar;
 
 use List::Util qw{ first };
 use Tk::HList;
@@ -21,6 +22,7 @@ use Tk::PNG;
 use Tk::ROText;
 use Tk::Sugar;
 
+use Games::Risk::I18n  qw{ T };
 use Games::Risk::Utils qw{ $SHAREDIR };
 
 with 'Tk::Role::HasWidgets';
@@ -99,7 +101,7 @@ sub START {
 
         # create the actions
         my @enabled  = qw{ new quit };
-        my @disabled = qw{ close };
+        my @disabled = qw{ close show_cards show_continents };
         foreach my $what ( @enabled, @disabled ) {
             my $action = Tk::Action->new(
                 window   => $mw,
@@ -128,9 +130,125 @@ sub START {
         $mw->minsize($mw->width, $mw->height);
     }
 
-    sub _build_toolbar {}
-    sub _build_menubar {}
+    #
+    # $main->_build_menubar;
+    #
+    # create the window's menu.
+    #
+    sub _build_menubar {
+        my $self = shift;
+        my $s = $self->_session;
+
+        # no tear-off menus
+        $mw->optionAdd('*tearOff', 'false');
+
+        my $menubar = $mw->Menu;
+        $mw->configure(-menu => $menubar );
+        $self->_set_w('menubar', $menubar);
+
+        # menu game
+        my @mnu_game = (
+        [ 'new',   'filenew16',   'Ctrl+N', T('~New game')   ],
+        #[ 'load',  'fileopen16',  'Ctrl+O', T('~Load game')  ],
+        [ 'close', 'fileclose16', 'Ctrl+W', T('~Close game') ],
+        [ '---'                                              ],
+        [ 'quit',  'actexit16',   'Ctrl+Q', T('~Quit')       ],
+        );
+        $self->_build_menu('game', T('~Game'), @mnu_game);
+
+        # menu view
+        my @mnu_view = (
+        [ 'show_cards',      '', 'F5', T('~Cards') ],
+        [ 'show_continents', '', 'F6', T('C~ontinents') ],
+        );
+        $self->_build_menu('view', T('~View'), @mnu_view);
+    }
+
+    #
+    # $self->_build_menu( $mnuname, $mnulabel, @submenus );
+    #
+    # Create the menu $label, with all the @submenus.
+    # @submenus is a list of [$name, $icon, $accel, $label] items.
+    # Store the menu items under the name menu_$mnuname_$name.
+    #
+    sub _build_menu {
+        my ($self, $mnuname, $mnulabel, @submenus) = @_;
+        my $menubar = $self->_w('menubar');
+        my $s = $self->_session;
+
+        my $menu = $menubar->cascade(-label => $mnulabel);
+        foreach my $item ( @submenus ) {
+            my ($name, $icon, $accel, $label) = @$item;
+
+            # separators are easier
+            if ( $name eq '---' ) {
+                $menu->separator;
+                next;
+            }
+
+            # regular buttons
+            my $action = $self->_action($name);
+            my $widget = $menu->command(
+                -label       => $label,
+                -image       => $icon,
+                -compound    => 'left',
+                -accelerator => $accel,
+                -command     => $action->callback,
+            );
+            $self->_set_w("menu_${mnuname}_${name}", $widget);
+
+            # create the bindings. note: we also need to bind the lowercase
+            # letter too!
+            $action->add_widget($widget);
+            $accel =~ s/Ctrl\+/Control-/;
+            $action->add_binding("<$accel>");
+            $accel =~ s/Control-(\w)/"Control-" . lc($1)/e;
+            $action->add_binding("<$accel>");
+        }
+    }
+
+    #
+    # $main->_build_toolbar;
+    #
+    # create the window toolbar (the one just below the menu).
+    #
+    sub _build_toolbar {
+        my $self = shift;
+        my $session = $self->_session;
+
+        # create the toolbar
+        my $tb = $mw->ToolBar( -movable => 0, top );
+        $self->_set_w('toolbar', $tb);
+
+        # the toolbar widgets
+        my @tb = (
+            [ 'Button', 'filenew22',   'new',   T('New game')   ],
+            #[ 'Button', 'fileopen22',  'load',  T('Load game')  ],
+            [ 'Button', 'fileclose22', 'close', T('Close game') ],
+            [ 'Button', 'actexit22',   'quit',  T('Quit')       ],
+        );
+
+        # create the widgets
+        foreach my $item ( @tb ) {
+            my ($type, $image, $name, $tip) = @$item;
+
+            # separator is a special case
+            $tb->separator( -movable => 0 ), next if $type eq 'separator';
+            my $action = $self->_action($name);
+
+            # regular toolbar widgets
+            my $widget = $tb->$type(
+                -image       => $image,
+                -tip         => $tip,
+                -command     => $action->callback,
+            );
+            $self->_set_w( "tbut_$name", $widget );
+            $action->add_widget( $widget );
+        }
+    }
+
     sub _build_canvas {}
+
 }
 
 no Moose;
