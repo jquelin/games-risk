6d34cb3237a938c76f47d6997cd22844aed52add
Jerome Quelin <jquelin@gmail.com>
2011-09-14 17:10:48 +0200

    gr:continent moose-ified

--- lib/Games/Risk/Continent.pm
+++ lib/Games/Risk/Continent.pm
@@ -6,57 +6,63 @@ package Games::Risk::Continent;
 # ABSTRACT: continent object
 
 use List::MoreUtils qw{ all };
+use Moose;
+use MooseX::Has::Sugar;
+use MooseX::SemiAffordanceAccessor;
 
-use base qw{ Class::Accessor::Fast };
-__PACKAGE__->mk_accessors( qw{ id bonus name _countries } );
+
+# -- attributes
+
+=attr id
+
+Unique id assigned to the continent.
+
+=attr bonus
+
+Number of bonus armies given when a player controls every country in the
+continent.
+
+=attr name
+
+Continent name.
+
+=attr color
+
+Color of the continent to flash it.
+
+=attr map
+
+Reference to the parent map (weak ref to a L<Games::Risk::Map> object).
+
+=attr countries
+
+The L<Games::Risk::Country> objects belonging to this continent.
+
+=cut
+
+has id    => ( ro, isa=>'Int', required );
+has name  => ( ro, isa=>'Str', required );
+has bonus => ( ro, isa=>'Int', required );
+has color => ( ro, isa=>'Str', required );
+has map   => ( ro, isa=>'Games::Risk::Map', required, weak_ref );
+has countries => ( rw, auto_deref, isa=>'ArrayRef[Games::Risk::Country]' );
 
 
-#--
-# METHODS
+# -- finalizer
+
+sub DEMOLISH {  debug( "~continent " . $_[0]->name ."\n" ); }
+
 
 # -- public methods
 
-#
-# $continent->add_country( $country );
-#
-# Store C<$country> (a C<Games::Risk::Country> object) as a country
-# located within the continent.
-#
-sub add_country {
-    my ($self, $country) = @_;
-    my $countries = $self->_countries // [];
-    push @$countries, $country;
-    $self->_countries($countries);
-}
+=method is_owned
 
-#
-# my @countries = $continent->countries;
-#
-# Return the list of countries located in $continent.
-#
-sub countries {
-    my ($self) = @_;
-    return @{ $self->_countries // [] };
-}
+    my $p0wned = $continent->is_owned( $player );
 
+Return true if C<$player> is the owner of all C<$continent>'s countries.
 
-#
-# $continent->destroy;
-#
-# Remove all circular references of $continent, to prevent memory leaks.
-#
-#sub DESTROY { say "destroy: $_[0]"; }
-sub destroy {
-    my ($self) = @_;
-    $self->_countries([]);
-}
+=cut
 
-
-#
-# my $p0wned = $continent->is_owned( $player );
-#
-# Return true if $player is the owner of all $continent's countries.
-#
 sub is_owned {
     my ($self, $player) = @_;
 
@@ -64,96 +70,16 @@ sub is_owned {
 }
 
 
+__PACKAGE__->meta->make_immutable;
 1;
-
 __END__
 
-
-
-=head1 SYNOPSIS
-
-    my $id = Games::Risk::Continent->new(\%params);
-
-
+=for Pod::Coverage
+    DEMOLISH
 
 =head1 DESCRIPTION
 
 This module implements a map continent, with all its characteristics.
-
-
-
-=head1 METHODS
-
-=head2 Constructor
-
-
-=over 4
-
-=item * my $player = Games::Risk::Continent->new( \%params )
-
-Create a new continent. Mandatory params are C<id>, C<name> and C<bonus>
-(see below in C<Accessors> for a quick definition).
-
-
-=back
-
-
-=head2 Accessors
-
-The following accessors (acting as mutators, ie getters and setters) are
-available for C<Games::Risk::Continent> objects:
-
-
-=over 4
-
-=item * bonus()
-
-number of bonus armies given when a player controls every country in the
-continent.
-
-
-=item * id()
-
-unique id assigned to the continent.
-
-
-=item * name()
-
-continent name.
-
-=back
-
-
-=head2 Public methods
-
-=over 4
-
-=item * $continent->add_country( $country )
-
-Store C<$country> (a C<Games::Risk::Country> object) as a country
-located within the C<$continent>.
-
-
-=item * $continent->destroy()
-
-Remove all circular references of C<$continent>, to prevent memory leaks.
-
-
-=item * my @countries = $continent->countries()
-
-Return the list of countries located in C<$continent>.
-
-
-=item * my $p0wned = $continent->is_owned( $player )
-
-Return true if C<$player> is the owner of all C<$continent>'s countries.
-
-
-=back
-
-
-
-=head1 SEE ALSO
-
-L<Games::Risk>.
+The word continent is a bit loose, since for some maps it can be either
+a region, a suburb... or a planet! :-)
 
