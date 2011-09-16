455a33acf9db5ba7a58d7aa65180d4930a599417
Jerome Quelin <jquelin@gmail.com>
2011-09-16 13:49:20 +0200

    new methods: all, add, del

--- lib/Games/Risk/Deck.pm
+++ lib/Games/Risk/Deck.pm
@@ -23,31 +23,64 @@ sub DEMOLISH { debug( "~deck: $_[0]\n" ); }
 
 The set of L<Games::Risk::Card>s hold in the deck.
 
+
+=method all
+
+    my @cards = $deck->cards;
+    my @cards = $deck->all;
+
+Return all the cards in the C<$deck>.
+
 =method get
 
     my $card = $deck->get;
 
 Get the next C<$card> in the deck.
 
+=method add
+
 =method return
 
-    $deck->return( $card ).
+    $deck->add( $card );
+    $deck->return( $card );
 
 Return C<$card> to the deck of cards.
 
 =cut
 
 has cards => (
-    ro, required, auto_deref,
+    ro, auto_deref,
+    default    => sub { [] },
     traits     => ['Array'],
     isa        => 'ArrayRef[Games::Risk::Card]',
     handles => {
 #        clear   => 'clear',
 #        count   => 'count',
+        all     => 'elements',
         get     => 'shift',
         return  => 'push',
+        add     => 'push',
+        _firstidx => 'first_index',
+        _delete   => 'delete',
     },
 );
+
+
+# -- public methods
+
+=method del
+
+    $deck->del( $card );
+
+Remove a C<$card> from the deck.
+
+=cut
+
+sub del {
+    my ($self, $card) = @_;
+    my $idx = $self->_firstidx( sub { $_[0] eq $card } );
+    $self->_delete( $idx );
+}
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
